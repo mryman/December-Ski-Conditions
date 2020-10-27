2436bcc47de01a7caa5e287424fb75d31c17ec33
Mark Ryman <mark.ryman@gmail.com>
2020-10-27 14:43:50 -0700

    cleaned up outliers in data and began first visualizations

--- openingDates.py
+++ openingDates.py
@@ -7,3 +7,15 @@ Season2018 = '2018-12-22'
 Season2019 = '2020-01-05'
 
 seasons = [Season2015, Season2016, Season2017, Season2018, Season2019]
+
+
+#Critical target date to open for Holiday season:
+
+xmas2015 = '2015-12-19'
+xmas2016 = '2016-12-17'
+xmas2017 = '2017-12-16'
+xmas2018 = '2018-12-22'
+xmas2019 = '2019-12-21'
+
+crit_dates = [xmas2015, xmas2016, xmas2017, xmas2018, xmas2019]
+
