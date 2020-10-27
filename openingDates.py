1a1fbc6488cb26a2693db656534b96590a26040b
Mark Ryman <mark.ryman@gmail.com>
2020-10-27 08:25:14 -0700

    moved data files to subdirectory and saved notebook

---
+++ openingDates.py
@@ -0,0 +1,9 @@
+#Opening date for each year at Alpental ski area
+
+Season2015 = '2015-12-18'
+Season2016 = '2016-12-12'
+Season2017 = '2017-12-09'
+Season2018 = '2018-12-22'
+Season2019 = '2020-01-05'
+
+seasons = [Season2015, Season2016, Season2017, Season2018, Season2019]
