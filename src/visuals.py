a940f8835b534fccb5522cf663d9e178618eb183
Mark Ryman <mark.ryman@gmail.com>
2020-10-30 12:00:09 -0700

    started py file for visualizations

---
+++ src/visuals.py
@@ -0,0 +1,94 @@
+import matplotlib.pyplot as plt  
+import pandas as pd  
+import datetime as dt
+import scipy.stats as stats
+
+
+def plt_precip_totals(df):
+    '''
+    Create visualization illustrating quantity of month total rain vs. snow, grouped by Year
+
+    ARG: df pertaining to specific month
+    OUTPUT: jpg file
+    '''
+    rain = df.groupby(df['dt_time_pst'].dt.year)['hrly_rain'].sum()
+    snow = df.groupby(df['dt_time_pst'].dt.year)['hrly_snow'].sum()
+
+    fig, ax = plt.subplots(figsize=(10,4))
+    ax.bar(rain.index, rain, width=0.25, label='Rain')
+    ax.bar(snow.index + 0.25, snow, width=0.25, label='Snow')
+    ax.set_ylabel('Precip in In. Water Equivalent')
+    ax.legend()
+    plt.suptitle('December Cumulative Precipitation by Type',y=0.97,fontsize=18, fontweight='bold')
+
+    return plt.savefig('img/precip_totals.jpg')
+
+
+def plt_precip_means(df):
+    
+    '''
+    Plot normal distribution graphs of precipitation types and show mean for each
+
+    ARG: df - month df with columns added for snow and rain
+    OUTPUT: jpg file
+    '''
+    
+    full_rain_mean = df['hrly_rain'].mean()
+    full_rain_sd = df['hrly_rain'].std()
+
+    full_snow_mean = df['hrly_snow'].mean()
+    full_snow_sd = df['hrly_snow'].std()
+
+
+    x_min = -0.2
+    x_max = 0.2
+    x = np.linspace(x_min, x_max, 100)
+
+    yrain = stats.norm.pdf(x,full_rain_mean,full_rain_sd)
+    ysnow = stats.norm.pdf(x,full_snow_mean, full_snow_sd)
+
+    fig, ax = plt.subplots(figsize=(10,4))
+
+    ax.plot(x,yrain, color='blue',label='Rain')
+    ax.plot(x,ysnow, color='coral', label='Snow')
+    plt.axvline(full_rain_mean, ls='--', lw=1, color='blue')
+    plt.axvline(full_snow_mean, ls='--', lw=1, color='coral')
+    ax.set_xlabel('Precip in In. Water Equivalent')
+    ax.legend()
+    return plt.savefig('img/daily_precip_means.jpg')
+
+def plt_temp_daily_means(df, month_name):
+     '''
+    Plot daily mean temps for each year in separate ax
+
+    ARG: df - month df 
+         month_name - string
+    OUTPUT: jpg file
+    '''
+    
+    
+    fig, axs = plt.subplots(5,1, figsize = (15, 10), sharex = True, sharey=True)
+    for i, year in enumerate([2015, 2016, 2017, 2018, 2019]):
+        #print(year)
+        x = df['dt_time_pst'].dt.day.unique()
+        yrmask = df['dt_time_pst'].dt.year == year
+        ymean = df[yrmask]['temperature_deg_f'].groupby(df['dt_time_pst'].dt.day).mean()
+        ymax = df[yrmask]['temperature_deg_f'].groupby(df['dt_time_pst'].dt.day).max()
+        ymin = df[yrmask]['temperature_deg_f'].groupby(df['dt_time_pst'].dt.day).min()
+    
+    
+        axs[i].set_ylabel('Temp Deg F')
+    
+        axs[i].plot(x, ymean, color='navy', lw=2, label=year)
+        axs[i].axis([1,32, 0, 45])
+        axs[i].text(6, 6, year, style='italic', fontsize=16)
+   
+        axs[i].grid()
+        axs[i].axhline(32, 0,31, ls='--', lw=1, color='red')
+    
+    axs[4].set_xlabel(month_name + 'Dates', fontsize=12)    
+    plt.suptitle('Mean Daily Temps for ' + month_name + ' Dates',y=0.97,fontsize=18, fontweight='bold')
+    plt.text(0.14, 0.9, '----- Rain/Snow Transition Temp', fontsize=12, color='red',transform=plt.gcf().transFigure)
+    plt.text(0.68, 0.9, 'Data from instruments at 3100ft elev', fontsize=12, transform=plt.gcf().transFigure)
+
+    return plt.savefig('img/meantemps.jpg')
